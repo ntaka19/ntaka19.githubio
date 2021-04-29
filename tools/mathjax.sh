34f96d3c6382382ad0b069a019411a0e6939de2d
Naruaki Takano <naruakitakano@gmail.com>
2021-04-29 18:20:55 +0900

    update repo 2021年 4月29日 木曜日 18時20分55秒 JST

--- tools/mathjax.sh
+++ tools/mathjax.sh
@@ -1,4 +1,5 @@
 sed "s/\\\begin{equation}/\$\$/g" $1 | sed "s/\\\end{equation}/\$\$/g" |
 sed "s/\\\begin{itemize}//g" | sed "s/\\\end{itemize}//g" |
 sed "s/\\\begin{enumerate}//g" | sed "s/\\\end{enumerate}//g" | 
-sed "s/\\\subsection{/## /g" |  sed "s/\\\}//g" > tt.dat
+sed "s/\\\item/- /g" |  
+sed "s/\\\subsection{/## /g" |  sed "s/}//g" > $1
